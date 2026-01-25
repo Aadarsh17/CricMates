a032a1c115bf5eee9155f2c8a1c3c850cd5d43d0
Aadarsh Pandey <aadarshpandey.ap863@gmail.com>
2026-01-25 19:07:39 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAZ74FOfIe5KtMt9-n9r9ucKpVBiDM6IDg",
   authDomain: "cricmates-tca.firebaseapp.com",
   projectId: "cricmates-tca",
-  storageBucket: "cricmates-tca.appspot.com",
+  storageBucket: "cricmates-tca.firebasestorage.app",
   messagingSenderId: "653804136264",
   appId: "1:653804136264:web:7a3def9e07dd3955ff357d",
   measurementId: "G-Q5RXY3PWXC"
