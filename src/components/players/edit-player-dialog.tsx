c8834d30698bff942f0b07fcc83403a840284709
Aadarsh Pandey <aadarshpandey.ap863@gmail.com>
2026-02-01 05:31:20 +0000

    Same issue

--- src/components/players/edit-player-dialog.tsx
+++ src/components/players/edit-player-dialog.tsx
@@ -66,14 +66,16 @@ export function EditPlayerDialog({ player, onPlayerEdit }: EditPlayerDialogProps
   });
 
   useEffect(() => {
-    form.reset({
-      name: player.name,
-      role: player.role,
-      battingStyle: player.battingStyle || 'Right-hand bat',
-      bowlingStyle: player.bowlingStyle || 'None',
-      isWicketKeeper: player.isWicketKeeper || false,
-    });
-  }, [player, form]);
+    if (open) {
+      form.reset({
+        name: player.name,
+        role: player.role,
+        battingStyle: player.battingStyle || 'Right-hand bat',
+        bowlingStyle: player.bowlingStyle || 'None',
+        isWicketKeeper: player.isWicketKeeper || false,
+      });
+    }
+  }, [player, open, form]);
 
   function onSubmit(values: PlayerFormData) {
     onPlayerEdit(values);
