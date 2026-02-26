1cc4ea60bac2e7203d2ba57abaa7d479ecda638c
Aadarsh17 <78806440+Aadarsh17@users.noreply.github.com>
2026-02-26 06:35:43 -0500

    config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-377718821-28e23",
-  "appId": "1:729093055321:web:19d6b0552f4235d8a92248",
-  "apiKey": "AIzaSyC9F8AIkTh6y6OpiteM16hHeCfEbRlwNrM",
-  "authDomain": "studio-377718821-28e23.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "729093055321"
+  projectId: "studio-377718821-28e23",
+  appId: "1:729093055321:web:19d6b0552f4235d8a92248",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "studio-377718821-28e23.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "729093055321"
 };
