5766c1f014d7487c9ef278a9b95328bdd27be8d1
Aadarsh Pandey <aadarshpandey.ap863@gmail.com>
2026-02-28 14:27:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7716611890-5bc21",
+  "appId": "1:821823513520:web:77e264a1ae72bfca216c75",
+  "apiKey": "AIzaSyARhRj1Elh_f0XY-KRRXvIFxkocq-iuhcY",
+  "authDomain": "studio-7716611890-5bc21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "821823513520"
+};
